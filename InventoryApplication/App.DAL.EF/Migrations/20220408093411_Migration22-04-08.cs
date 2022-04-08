56987adc83ab2c55e893821676bb563160d44fb3
user <rvarbl@ttu.ee>
2022-04-08 12:34:58 +0300

    authentication

---
+++ InventoryApplication/App.DAL.EF/Migrations/20220408093411_Migration22-04-08.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.DAL.EF.Migrations
+{
+    public partial class Migration220408 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
