61fa7759fb9cd1fa5ee42c3cb424f7db971a0c6d
user <rvarbl@ttu.ee>
2022-04-10 14:53:11 +0300

    client

--- app_ui/src/main.ts
+++ app_ui/src/main.ts
@@ -1,10 +1,12 @@
+import 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 import Aurelia, { RouterConfiguration } from 'aurelia';
 import { MyApp } from './my-app';
 
 Aurelia
   .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
