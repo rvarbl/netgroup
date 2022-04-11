6a884269bded17914349681e0ec0732b57400657
user <rvarbl@ttu.ee>
2022-04-11 15:31:40 +0300

    inventory

--- app_ui/src/views/user-view/user-view.ts
+++ app_ui/src/views/user-view/user-view.ts
@@ -1,16 +1,20 @@
+import { IRouter } from "aurelia";
 import { IStorage } from "../../domain/inventory/IStorage";
 import { AppState } from "../../state/AppState";
 
 export class UserView {
     storages: IStorage[] | undefined;
-    constructor(private appState: AppState) {
+    constructor(private appState: AppState, @IRouter private router: IRouter) {
         this.storages = [
-             {ChildStorage:undefined, ParentStorageId:"123", StorageName: "name"}
+            { ChildStorage: undefined, ParentStorageId: "123", StorageName: "name" }
         ]
     }
 
-    private getAllStorages() {
+    getAllStorages() {
         this.storages = this.appState.getAllStorages()
         console.log("GETALL + " + this.storages);
+    }
+    async detailView(id: string) {
+        await this.router.load(`./storage-detail-view`);
     }
 }
